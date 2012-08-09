195c8794723a7def0bc82f27f820f6572c8c7ffd
lauremegan <lauremegan@hotmail.com>
2012-08-09 19:05:00 +0100

    tried_Image_uploader Video

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,30 @@
 CommunityApp::Application.routes.draw do
-   get "users/new"
-
   root to: 'static_pages#home'
 
+   get "users/new"
+   
+  
+  resources :users
+  resources :sessions, only: [:new, :create, :destroy]
+
   match '/signup',  to: 'users#new'
+  match '/signin',  to: 'sessions#new'
+  match '/signout', to: 'sessions#destroy', via: :delete
+
+  
+
+  #match '/signup',  to: 'users#new'
 
   match '/help',    to: 'static_pages#help'
   match '/about',   to: 'static_pages#about'
   match '/contact', to: 'static_pages#contact'
+ 
+resources :videos
+resources :galleries
+resources :paintings
+ # resources :users
+ # resources :sessions
 
-  resources :users
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
